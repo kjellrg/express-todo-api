e344b6bdf4fa465ba39cd4b4e84bf0383c5705f4
Kjell Rune Glærum <kjellrg@photic.net>
2022-07-08 16:08:49 +0200

    Change directory structure

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-const loginRoute = require('./routes/todos')
-const todosRoute = require('./routes/todos')
+const loginRoute = require('./routes/api/v1/login')
+const todosRoute = require('./routes/api/v1/todos')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/todos', todosRoute)
-app.use('/login', loginRoute)
+app.use('/api/v1/todos', todosRoute)
+app.use('/api/v1/login', loginRoute)
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`)
