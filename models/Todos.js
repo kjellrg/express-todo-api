ccdd23c33f85e695cab6260e74fec4c3367d723a
kjellrg <kjellrg@photic.net>
2022-07-15 16:49:50 +0000

    Unique title

--- models/Todos.js
+++ models/Todos.js
@@ -1,11 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const TodoSchema = mongoose.Schema({
-    title: { type: String, minlength: 5, maxlength: 24, required: true },
+const TodoSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      minlength: 5,
+      maxlength: 24,
+      required: true,
+      unique: true,
+    },
     body: { type: String, minlength: 10, maxlength: 128 },
-    completed: { type: Boolean, default: false, required: true}
-}, {
-    timestamps: true
-});
+    completed: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+    strict: true,
+  }
+);
 
-module.exports = mongoose.model('TodoList', TodoSchema);
+module.exports = mongoose.model("TodoList", TodoSchema);
